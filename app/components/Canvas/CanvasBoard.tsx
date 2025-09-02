3fa8b62e3e7583949d8e4a6fd4df92c9f01ab087
Dhruva16630 <dhruva6kumar1@gmail.com>
2025-09-02 23:24:00 +0530

    Added_rectangle

--- app/components/Canvas/CanvasBoard.tsx
+++ app/components/Canvas/CanvasBoard.tsx
@@ -36,8 +36,14 @@ export default function CanvasBoard() {
         className="w-screen h-screen bg-[#121212]"
       />
       <div className="absolute top-8 left-96 gap-2 rounded-2xl bg-white flex p-2">
-        <button onClick={() => engine?.drawRectangle(200, 200, 200, 200)}>
+        <button onClick={() => engine?.drawRectangle(300, 300, 300, 300)}>
           Rect
+        </button>
+        <button onClick={() => engine?.drawCircle(500, 500, 150)}>
+          Circle
+        </button>
+        <button onClick={() => engine?.drawDiamond(700, 300, 300, 300)}>
+          Diamond
         </button>
       </div>
     </div>
--- app/components/Canvas/CanvasLogic.ts
+++ app/components/Canvas/CanvasLogic.ts
@@ -15,9 +15,21 @@ class CanvasLogic {
     }
     
     drawRectangle( x:number, y:number, width:number, height:number) {
-        const rectangle = this.roughCanvas.rectangle(200, 200, 200, 200, {stroke: "white", strokeWidth: 1,roughness: 1.3});
+        const rectangle = this.roughCanvas.rectangle(x, y, width, height, {stroke: "white", strokeWidth: 1,roughness: 1.3});
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.roughCanvas.draw(rectangle);
+    }
+
+    drawCircle(x:number, y:number, radius:number) {
+        const circle = this.roughCanvas.circle(x, y, radius, {stroke: "white", strokeWidth: 1,roughness: 1.3});
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.roughCanvas.draw(circle);
+    }
+
+    drawDiamond(x:number, y:number, width:number, height:number) {
+        const diamond = this.roughCanvas.polygon([[x, y - height / 2], [x + width / 2, y], [x, y + height / 2], [x - width / 2, y]], {stroke: "white", strokeWidth: 1,roughness: 1.3});
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.roughCanvas.draw(diamond);
     }
 }
 
