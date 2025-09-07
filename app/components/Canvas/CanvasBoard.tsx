80a853a9a1d5429d1764f7bffa22b65de05d180d
Dhruva16630 <dhruva6kumar1@gmail.com>
2025-09-07 22:26:39 +0530

    Cleaned_Buttons

--- app/components/Canvas/CanvasBoard.tsx
+++ app/components/Canvas/CanvasBoard.tsx
@@ -9,6 +9,17 @@ export default function CanvasBoard() {
   const [engine, setEngine] = useState<CanvasLogic | null>(null);
   const [tool, setTool] = useState<Tool>(null);
 
+
+  const tools: { key:string, value:string }[]= [
+    {key:"rectangle", value:"Rect"},
+    {key:"circle", value:"Circle"},
+    {key:"diamond", value:"Diamond"},
+    {key:"pencil", value:"Pencil"},
+    {key:"line", value:"Line"},
+    {key:"arrow", value:"Arrow"},
+    {key:"text", value:"Write"},
+  ]
+
   useEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
@@ -21,9 +32,9 @@ export default function CanvasBoard() {
     const engineInstance = new CanvasLogic(canvas);
     setEngine(engineInstance);
 
-  
 
-  
+
+
 
     const handleResize = () => {
       canvas.width = window.innerWidth;
@@ -31,17 +42,20 @@ export default function CanvasBoard() {
     };
     window.addEventListener("resize", handleResize);
 
-    
-    
-  }, []); 
 
-  useEffect(() =>{
-    if(engine && tool){
+
+  }, []);
+
+  useEffect(() => {
+    if (engine && tool) {
       engine.setTool(tool);
     }
 
   }, [tool, engine])
 
+  
+
+  
 
   return (
     <div className="relative">
@@ -49,29 +63,18 @@ export default function CanvasBoard() {
         ref={canvasRef}
         className="w-screen h-screen bg-[#121212]"
       />
-      <div className="absolute top-8 left-96 -translate-1/2 gap-2 rounded-2xl bg-white flex p-2">
-        <button onClick={() =>{setTool("rectangle")}} >
-          Rect
-        </button>
-        <button onClick={()=>{setTool("circle"), console.log("tool set to circle")}}>
-          Circle
-        </button>
-        <button onClick={()=>{setTool("diamond")}}>
-          Diamond
-        </button>
-        <button onClick={()=>{setTool("line")}}>
-          Line
-        </button>
-        <button onClick={()=>{setTool("arrow")}}>
-          Arrow
-        </button>
-        <button onClick={()=>{setTool("pencil")}}>
-          Pencil
-        </button>
-        <button onClick={()=>{setTool("text")}}>
-          Write
-        </button>
-        
+      <div className="fixed top-5 w-full flex justify-center">
+        <div className="gap-2  rounded-2xl bg-white flex p-2">
+          {tools.map(({ key ,value }) =>(
+            <button
+              key = {key}
+              onClick = {() => setTool(key as Tool)}
+              className={`px-3 py-1 rounded-lg transition ${ tool === key ? "bg-blue-500 text-white" : "bg-gray-100 text-gray-800 hover:bg-gray-200"}`}
+            >
+              {value}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
