886e7440e84e731ec6bfed18d1fc318a1a00a676
Dhruva16630 <dhruva6kumar1@gmail.com>
2025-09-19 20:59:59 +0530

    Added social links

--- app/components/MenuBar.tsx
+++ app/components/MenuBar.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 
 
-
-
-
 const MenuBar = () => {
     
     return (
         <div className='fixed left-4 top-14 bg-[#232329] text-gray-300 p-3 rounded-lg flex flex-col gap-2 '>
-            <h5><i className='ri-github-fill mr-3 text-xl'></i>Github</h5>
-            <h5><i className='ri-twitter-x-fill mr-3 text-lg'></i>X ( Twitter )</h5>
-            <h5><i className='ri-linkedin-box-fill mr-3 text-xl '></i>Linkedin</h5>
+            <a href='https://github.com/Dhruva16630' target='_blank'><h5><i className='ri-github-fill mr-3 text-xl'></i>Github</h5></a>
+            <a href='https://x.com/chandru_dh24491' target='_blank'><h5><i className='ri-twitter-x-fill mr-3 text-lg'></i>X ( Twitter )</h5></a>
+            <a href='https://www.linkedin.com/in/dhruva-kumar-c-041491264/' target='_blank'><h5><i className='ri-linkedin-box-fill mr-3 text-xl '></i>Linkedin</h5></a>
             <hr className='border-gray-700' />
             {/* <div className='flex flex-row items-center'>
                 Theme 
